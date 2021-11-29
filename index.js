1e871b5e90f0cafb09daff295bdbe2efd66c1b64
Leah <elloworldt@gmail.com>
2021-11-28 17:27:33 -0800

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(number) {
+  return Math.abs(number - 42)
+  }
+
+
+  function distanceFromHqInFeet(feet) {
+    return distanceFromHqInBlocks(feet) * 264
+  }
+
+
+  function distanceTravelledInFeet(start, destination) {
+    return distanceFromHqInFeet(destination) - distanceFromHqInFeet(start);
+    //returns the number of feet traveled
+  }
+
+  function calculatesFarePrice(start, destination) {
+    let totalDistance = (destination - start)
+    if { totalDistance <= 400
+      return 0
+    } else { totalDistance >= 400
+      return totalDistance * .2
+    } else    }
+
+}
